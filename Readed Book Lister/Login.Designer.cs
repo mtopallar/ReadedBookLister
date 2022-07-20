e6505786a8a6f99f50d02355f2f1ba5ffa86f53b
Murat Topallar <topallarm@outlook.com>
2022-07-20 15:16:00 +0300

    Login form geliştiriliyor.

---
+++ Readed Book Lister/Login.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Readed_Book_Lister
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.pnlUserName = new System.Windows.Forms.Panel();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Trebuchet MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblLogin.ForeColor = System.Drawing.Color.White;
+            this.lblLogin.Location = new System.Drawing.Point(97, 80);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(147, 61);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Login";
+            // 
+            // pnlUserName
+            // 
+            this.pnlUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlUserName.BackColor = System.Drawing.Color.White;
+            this.pnlUserName.Location = new System.Drawing.Point(53, 228);
+            this.pnlUserName.Name = "pnlUserName";
+            this.pnlUserName.Size = new System.Drawing.Size(235, 1);
+            this.pnlUserName.TabIndex = 1;
+            // 
+            // tbxName
+            // 
+            this.tbxName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbxName.BackColor = System.Drawing.Color.DarkGreen;
+            this.tbxName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxName.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbxName.ForeColor = System.Drawing.Color.White;
+            this.tbxName.Location = new System.Drawing.Point(53, 207);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.PlaceholderText = "Nickname";
+            this.tbxName.Size = new System.Drawing.Size(235, 18);
+            this.tbxName.TabIndex = 0;
+            this.tbxName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.BackColor = System.Drawing.Color.DarkGreen;
+            this.tbxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxPassword.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbxPassword.ForeColor = System.Drawing.Color.White;
+            this.tbxPassword.Location = new System.Drawing.Point(54, 267);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.PlaceholderText = "Password";
+            this.tbxPassword.Size = new System.Drawing.Size(235, 18);
+            this.tbxPassword.TabIndex = 3;
+            this.tbxPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxPassword.UseSystemPasswordChar = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(54, 288);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(235, 1);
+            this.panel1.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGreen;
+            this.ClientSize = new System.Drawing.Size(342, 498);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.pnlUserName);
+            this.Controls.Add(this.lblLogin);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Panel pnlUserName;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
